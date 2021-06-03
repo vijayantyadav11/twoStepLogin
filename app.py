452698bc11445646066ed4a843bc9c13a8db8108
Vijayant <46192981+vijayantyadav11@users.noreply.github.com>
2021-06-03 20:21:31 +0530

    digSet

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ class User:
                     return response
                 
                 passMsg=""
-                digSet=set([0,1,2,3,4,5,6,7,8,9])
+                digSet=set(['0','1','2','3','4','5','6','7','8','9'])
                 speSet=set(['$','&','@','#','%'])
                 passcheck=[False, False]
                 for ele in userpass:
